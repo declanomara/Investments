2c8691f87daf85db995b8296110a504bc36e31a9
declanomara <25646795+declanomara@users.noreply.github.com>
2023-07-23 19:30:27 -0400

    Minimal example of using quantlib to stream prices

--- trading/src/main.rs
+++ trading/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use quantlib;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let settings = quantlib::util::read_settings()?;
+    let price_stream = quantlib::oanda::PriceStream::new(&["EUR_USD".to_string()], &settings.oanda).await;
+
+    for item in price_stream {
+        // Match on the item to see what kind of stream item it is, if it's a price, print it out, otherwise ignore it
+        match item {
+            Ok(quantlib::oanda::StreamItem::Price(price)) => println!("{}: {}", price.instrument, price.closeout_bid),
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
