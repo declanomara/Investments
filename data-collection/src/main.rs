56321fa2d3e285b154e0286b99187a708818485c
declanomara <25646795+declanomara@users.noreply.github.com>
2023-09-08 17:24:16 -0400

    Added verbosity to data collection system.

--- data-collection/src/main.rs
+++ data-collection/src/main.rs
@@ -1,5 +1,29 @@
 use quantlib;
 
+// Ensure output directory exists
+fn validate_output_directory(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    // Create the directory if it doesn't exist
+    if !std::path::Path::new(path).exists() {
+        println!("[WARN] Output directory does not exist. Creating...");
+        std::fs::create_dir_all(path)?;
+    }
+
+    // Create the raw.log file within the output directory
+    let raw_log_path = format!("{}/raw.log", path);
+    if !std::path::Path::new(&raw_log_path).exists() {
+        println!("[INFO] Creating raw.log file...");
+        std::fs::File::create(&raw_log_path)?;
+    }
+
+    // Create bin/ directory within the output directory
+    let bin_path = format!("{}/bin", path);
+    if !std::path::Path::new(&bin_path).exists() {
+        println!("[INFO] Creating bin/ directory...");
+        std::fs::create_dir_all(&bin_path)?;
+    }
+    Ok(())
+}
+
 fn handle_error(e: Box<dyn std::error::Error>) {
     match e.downcast_ref::<tokio::time::error::Elapsed>() {
         Some(_elapsed_error) => {
@@ -16,6 +40,9 @@ fn handle_error(e: Box<dyn std::error::Error>) {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let log_path = "data/";
+    println!("[INFO] Validating output directory...");
+    validate_output_directory(log_path)?;
+
     let settings = quantlib::util::read_settings()?;
     let logging_price_stream = quantlib::oanda::LoggingPriceStream::new(
         &["EUR_USD".to_string()],
