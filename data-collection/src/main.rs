41e6c58f578ca63b0aa3fb44d7290ccccb57b4bb
declanomara <25646795+declanomara@users.noreply.github.com>
2023-09-08 16:33:13 -0400

    LoggingPriceStream now throws timeout error.

--- data-collection/src/main.rs
+++ data-collection/src/main.rs
@@ -1,25 +1,42 @@
 use quantlib;
 
-use tokio::time::timeout;
+fn handle_error(e: Box<dyn std::error::Error>) {
+    match e.downcast_ref::<tokio::time::error::Elapsed>() {
+        Some(_elapsed_error) => {
+            // Handle the elapsed error here
+            println!("[ERROR] Connection timed out.");
+        },
+        None => {
+            // Handle other errors here
+            println!("[ERROR] {}", e);
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let log_path = "data/";
     let settings = quantlib::util::read_settings()?;
-    let mut logging_price_stream = quantlib::oanda::LoggingPriceStream::new(
+    let logging_price_stream = quantlib::oanda::LoggingPriceStream::new(
         &["EUR_USD".to_string()],
         log_path,
+        5000,
         &settings.oanda
     ).await;
 
-    loop {
-        // Attempt to get new prices, but timeout after 5 seconds
-        let prices = timeout(
-            std::time::Duration::from_secs(5),
-            logging_price_stream.next_items()
-        ).await??;
-        println!("{:?}", prices);
+    for item in logging_price_stream {
+        match item {
+            Ok(quantlib::oanda::StreamItem::Price(price)) => {
+                println!("[{}][PRICE] Bid: {:.5} Ask: {:.5}", price.instrument, price.bid, price.ask);
+            },
+            Ok(quantlib::oanda::StreamItem::Heartbeat(_)) => {
+                // println!("[HEARTBEAT]");
+            },
+            Err(e) => {
+                handle_error(e);
+            }
+        }
     }
 
-    // Ok(())
+    Ok(())
 }
