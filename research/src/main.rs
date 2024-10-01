7abc4bb5cb32da5e912ed6353d1d05763acb7366
Declan O'Mara <25646795+declanomara@users.noreply.github.com>
2024-09-30 20:46:11 -0400

    Generic particle swarm optimizer

--- research/src/main.rs
+++ research/src/main.rs
@@ -1,38 +1,15 @@
-pub mod backtesting;
-pub mod optimization;
+mod optimization;
 
-use anyhow::Result;
-use quantlib::oanda::objects::Price;
-use rand::prelude::*;
+fn test_func(x: f64, y: f64) -> f64 {
+    (x - 1.0).powi(2) + (y - 2.0).powi(2)
+}
 
-const DATA_PATHS: [&str; 6] = [
-    "historical-data/weekly/EUR_USD/week-20.bin",
-    "historical-data/weekly/EUR_USD/week-21.bin",
-    "historical-data/weekly/EUR_USD/week-22.bin",
-    "historical-data/weekly/EUR_USD/week-23.bin",
-    "historical-data/weekly/EUR_USD/week-24.bin",
-    "historical-data/weekly/EUR_USD/week-25.bin",
-];
+fn fitness(arr: [f64; 2]) -> f64 {
+    test_func(arr[0], arr[1])
+}
 
-fn main() -> Result<()> {
-    // temp
-    thread_rng().gen_range(0..1);
-
-    // Load the data
-    let mut data_sets: Vec<Vec<Price>> = Vec::new();
-    for path in DATA_PATHS.iter() {
-        let data = backtesting::load_data(path)?;
-        data_sets.push(data);
-    }
-
-    // Randomize the order of the data sets
-    data_sets.shuffle(&mut thread_rng());
-
-    // Pick the first 3 data sets
-    data_sets.truncate(3);
-
-    // Run the optimization
-    optimization::optimize_ema(&data_sets)?;
-
-    Ok(())
+fn main() {
+    let initial = [100.0, 100.0];
+    let result = optimization::optimize(initial, fitness, 10.0, 10.0);
+    println!("Result: {:?}", result);
 }
